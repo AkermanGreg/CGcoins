3052b10674ab9e495469e5b8411c5a706f2efc98
AkermanGreg <gregoryakerman@gmail.com>
2014-12-06 14:06:48 -0800

    integrated visual with rails

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,43 +2,15 @@ class WelcomeController < ApplicationController
 
   def index
 
+    require 'blockchain'
 
+    @ticker_out = Array.new
+    ticker = Blockchain::get_ticker()
+    #print the 15 min price for every currency
+    ticker.keys.each do |key|
+       oneline = (ticker[key].p15min)
+       @ticker_out = oneline
+    end
 
-@handshake = WebSocket::Handshake::Client.new(url: 'ws://ws.blockchain.info/inv', headers: { 'Cookie' => 'SESSIONID=1234' })
-
-# Create request
-@handshake.to_s # GET /demo HTTP/1.1
-                # Upgrade: websocket
-                # Connection: Upgrade
-                # Host: example.com
-                # Cookie: SESSIONID=1234
-                # Sec-WebSocket-Origin: http://example.com
-                # Sec-WebSocket-Version: 13
-                # Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==
-
-# Parse server response
-@handshake << <<EOF
-HTTP/1.1 101 Switching Protocols\r
-Upgrade: websocket\r
-Connection: Upgrade\r
-Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=\r
-\r
-EOF
-
-# All data received?
-@handshake.finished?
-
-# No parsing errors?
-@handshake.valid?
-
-  end
-
-require 'blockchain'
-
-ticker = Blockchain::get_ticker()
-#print the 15 min price for every currency
-ticker.keys.each do |key|
-    puts ticker[key].p15min
-end
-
+    end
 end
