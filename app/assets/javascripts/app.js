c08646ae653f80e214188c729b9785c884d71bc6
thirdchance57 <thirdchance57@hotmail.com>
2014-12-10 17:34:40 -0800

    adding doge and litecoin websocket

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,7 @@
 var wsUri = "wss://ws.blockchain.info/inv";
 var btcOutput;
 var ltcOutput;
+var dgeOutput;
 // var output;
 var TYPE_BLOCK = "block";
 
@@ -9,24 +10,18 @@ function init() {
   initWebSocket();
 }
 
-function initWebSocket() {
-  //  init blockchain websocket (activity, blocks)
-  var blockchain = new WebSocket('ws://ws.blockchain.info/inv');
-  var litecoin = new WebSocket('ws://ws.dogechain.info/inv');
+function initWebSocket() {//  init blockchain websocket (activity, blocks)
   
-  litecoin.onopen = function () {
-
-    litecoin.send( JSON.stringify( {"op":"unconfirmed_sub"} ) );  //  subscribe to uncofirmed activity
-    litecoin.send( JSON.stringify( {"op":"blocks_sub"} ) );   //  subscribe to new blocks
- 
-  };
+ ///////////////////// BITCOIN STARTS///////////////////////
+  var blockchain = new WebSocket('ws://ws.blockchain.info/inv');
+  blockchain.onerror = function (error){ console.log('connection.onerror',error); };
   blockchain.onopen = function () {
+    console.log ("btc CONNECTED");
 
     blockchain.send( JSON.stringify( {"op":"unconfirmed_sub"} ) );  //  subscribe to uncofirmed activity
     blockchain.send( JSON.stringify( {"op":"blocks_sub"} ) );   //  subscribe to new blocks
   };
 
- ///////////////////// BITCOIN ///////////////////////
  // when messages is received turn it to json and pass it to message.data
   blockchain.onmessage = function (message) {
     var response = JSON.parse(message.data);
@@ -45,7 +40,7 @@ function initWebSocket() {
       // console.log(btcOutput);
 
       // PRINTS THE INITAL OUTPUT TO THE HTML        converts to string and back to integer 
-      // document.getElementById("output").innerHTML = "$" + (btcOutput.toString().match(/^\d+(?:\.\d{0,2})?/)) * 375;
+      document.getElementById("output").innerHTML = "$" + (btcOutput.toString().match(/^\d+(?:\.\d{0,2})?/)) * 375;
 
       // these if else sets the min and max size tokens for d3
       if( response.amount <= 5 ) {
@@ -75,19 +70,20 @@ function initWebSocket() {
         src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc-tI1b7luXJgfHdwbTwCmSpciFwSrVG_cqMlOeF-0CEzrS6NAxw'
       }
     });
-  }
+  }/////// end of BITCOIN ////////
+  
+  ///////////////////////////////// DOGECOIN STARTS////////////////////////////////////////
+  var dogecoin = new WebSocket('wss://ws.dogechain.info/inv');
+  dogecoin.onerror = function (error){ console.log('connection.onerror',error); };
+  dogecoin.onopen = function () {
+    console.log ("Doge Coin CONNECTED");
 
-  // function writeToScreen(message)
-  // {
-  //   var pre = document.createElement("p");
-  //   pre.style.wordWrap = "break-word";
-  //   pre.innerHTML = message;
-  //   output.appendChild(pre);
-  // }
+    dogecoin.send( JSON.stringify( {"op":"unconfirmed_sub"} ) );  //  subscribe to uncofirmed activity
+    dogecoin.send( JSON.stringify( {"op":"blocks_sub"} ) );   //  subscribe to new blocks
+  };
 
-  ///////////////////////////////// LITECOIN ////////////////////////////////////////
   // when messages is received turn it to json and pass it to message.data
-  litecoin.onmessage = function (message) {
+  dogecoin.onmessage = function (message) {
     var response = JSON.parse(message.data);
     console.log(message);
     
@@ -95,16 +91,16 @@ function initWebSocket() {
     if( response.op == "utx") {
       var amount = 0;
       
-      for(var i = 0; i < response.x.out.length; i++ )
-        amount += response.x.out[i].value;
+      for(var i = 0; i < response.x.value_out.length; i++ )
+        amount += response.x.value_out[i].value;
       
       // DIVIDES THE AMOUNT and COVERTS TO LTC
       response.amount = amount / 1;
-      ltcOutput = response.amount;
-      console.log(ltcOutput);
+      dgeOutput = response.amount;
+      console.log(dgeOutput);
 
       // PRINTS THE INITAL OUTPUT TO THE HTML        converts to string and back to integer 
-      // document.getElementById("output").innerHTML = "$" + (ltcOutput.toString().match(/^\d+(?:\.\d{0,2})?/)) * 375;
+      // document.getElementById("output").innerHTML = "$" + (dgeOutput.toString().match(/^\d+(?:\.\d{0,2})?/)) * 375;
 
       // these if else sets the min and max size tokens for d3
       if( response.amount <= 5 ) {
@@ -124,16 +120,77 @@ function initWebSocket() {
     // this function fires when an onmessage is received
     writeToScreen2(response.amount);
   };
-}
-function writeToScreen2(message) {
-  barChart.addToken( {
-    id:'myId',
-    size: message,
-    category:0,
-    texture: {
-      src: 'http://cdni.wired.co.uk/1920x1280/k_n/litecoin.jpg'
-    }
-  });
+
+  function writeToScreen2(message) {
+    barChart.addToken( {
+      id:'myId',
+      size: message,
+      category:0,
+      texture: {
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToSilQz7QztIRYp3ddniAdeaJQiCT7iDjlUvPlW4wtgE8pcw1_Cg'
+      }
+    });
+  }////// end of DOGECOIN //////
+
+// ///////////////////// LITECOIN starts///////////////////////
+//   var litecoin = new WebSocket('ws://ws.ltcchain.com:8000');
+//   litecoin.onerror = function (error){ console.log('connection.onerror',error); };
+//   litecoin.onopen = function () {
+//     console.log ("LiteCoin CONNECTED");
+
+//     litecoin.send( JSON.stringify( {"op":"tx_sub"} ) );  //  subscribe to uncofirmed activity
+//     litecoin.send( JSON.stringify( {"op":"block_sub"} ) );   //  subscribe to new blocks
+//   };
+
+//  // when messages is received turn it to json and pass it to message.data
+//   litecoin.onmessage = function (message) {
+//     var response = JSON.parse(message.data);
+//     console.log("LITECOIN IN MY ASS", message);
+    
+//     // unconfirmed transactions 
+//     if( response.op == "utx") {
+//       var amount = 0;
+      
+//       for(var i = 0; i < response.x.out.length; i++ )
+//         amount += response.x.out[i].value;
+      
+//       // DIVIDES THE AMOUNT and COVERTS TO BTC
+//       response.amount = amount / 100000000;
+//       ltcOutput = response.amount;
+//       console.log(ltcOutput);
+
+//       // PRINTS THE INITAL OUTPUT TO THE HTML        converts to string and back to integer 
+//       // document.getElementById("output").innerHTML = "$" + (ltcOutput.toString().match(/^\d+(?:\.\d{0,2})?/)) * 375;
+
+//       // these if else sets the min and max size tokens for d3
+//       if( response.amount <= 5 ) {
+//         response.amount = 5 ;
+//       }
+//       else if( response.amount >= 1400   ) {
+//         response.amount = 1400 ;
+//       }
+//     }
+
+//     // if a block is created
+//     else if( response.op == "block" ) {
+//       console.log("BLOCK FOUND BLOCK FOUND BLOCK FOUND BLOCK FOUND");
+//       response.amount = Math.round( response.x.height / 10000 );
+//     }
+    
+//     // this function fires when an onmessage is received
+//     writeToScreen3(response.amount);
+//   };
+//   // fires a function to drop a d3 token (message is the btc transaction size from response.amount)
+//   function writeToScreen3(message) {
+//     barChart.addToken( {
+//       id:'myId',
+//       size: message,
+//       category:0,
+//       texture: {
+//         src: 'http://www.wired.co.uk/news/archive/2013-06/16/litecoin/viewgallery/305201'
+//       }
+//     });
+//   } //////////// END OF LITECOIN /////////////
 }
 
 // fires init function when the window is loaded
