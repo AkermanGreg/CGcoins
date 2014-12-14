1e874016b610e136780b7bc784511c3fb51ae6d0
AkermanGreg <gregoryakerman@gmail.com>
2014-12-14 14:33:17 -0800

    integrated style

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,15 @@ class WelcomeController < ApplicationController
   def index
 
     require 'httparty'
-
     @response = HTTParty.get("https://blockchain.info/stats?format=json")
     @parsed_response = JSON.parse(@response.body)
-  
+  end
+
+  def bubble
+
+    require 'httparty'
+    @response = HTTParty.get("https://blockchain.info/stats?format=json")
+    @parsed_response = JSON.parse(@response.body)
   end
 
 end
