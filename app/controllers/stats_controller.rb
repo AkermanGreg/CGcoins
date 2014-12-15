5bb1caaf891898d0a27c95876a89baf72d745c75
AkermanGreg <gregoryakerman@gmail.com>
2014-12-15 14:21:40 -0800

    search api setup

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,10 +1,19 @@
 class StatsController < ApplicationController
-
+require 'httparty'
   def index
+    searcharg = params[:addressSearch]
+    if searcharg  && searcharg != ""
+      myURL = "https://blockchain.info/address/" + searcharg 
+      myURL = myURL + "format=json"
+      response = HTTParty.get(myURL)
+      @rbody = JSON.parse(response.body)
 
-    require 'httparty'
-    @response = HTTParty.get("https://blockchain.info/stats?format=json")
-    @parsed_response = JSON.parse(@response.body)
+      @inputs = @rbody["txs"][0]["inputs"]
+      @show_data = true
+    else 
+      @show_data = false
+    end
+
   end
 
 
